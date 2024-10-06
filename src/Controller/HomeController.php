553cd8c0aa0312c1c787a645f59623537006322c
Johan CLEMENT <johanclement@Johan-MacBook-Pro.local>
2024-10-06 21:07:17 +0200

    Premier commit. Initialisation du projet.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('/publicPages/homepage.html.twig', [
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+}
